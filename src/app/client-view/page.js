be4d9f8e00d526e691df6548578ea1de3ec55e2e
SamGreat <samantha@greatorex.org.uk>
2024-05-07 10:43:47 +0100

    Added image to client view

--- src/app/client-view/page.js
+++ src/app/client-view/page.js
@@ -1,10 +1,12 @@
 "use client";
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function ClientView() {
   return (
     <main className={styles.main}>
       <p>Client View</p>
+      <Image className={styles.image} alt="Zoom Products" src="/zoom-products.png" width={600} height={300} />
     </main>
   );
 }
